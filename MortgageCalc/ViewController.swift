6d021cd89b5436bc796786939a2327d7f2ebfae5
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2023-06-21 10:09:52 -0700

    Demonstrated how to setup constraints. Demonstrated how embedding our views in StackViews make it much easier to deal with constraints

--- MortgageCalc/ViewController.swift
+++ MortgageCalc/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var homeValueTF: UITextField!
+    
+    @IBOutlet weak var downPaymentTF: UITextField!
+    
+    @IBOutlet weak var loanAmountLabel: UILabel!
+    
+    var loanAmountValue = Int()
+    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
 
-
+    @IBAction func calculateButtonTapped(_ sender: Any) {
+        
+        guard let homeValueAmount = Int(homeValueTF.text ?? "") else {return}
+                
+        guard let downPaymentAmount = Int(downPaymentTF.text ?? "") else {return}
+       
+        loanAmountValue = homeValueAmount - downPaymentAmount
+        loanAmountLabel.text = "Loan amount is: \(loanAmountValue)"
+        loanAmountLabel.setNeedsLayout()
+    }
+    
 }
 
